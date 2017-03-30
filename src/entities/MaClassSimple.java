a6fdd9277762d874c6c4d3ba3921bff27f6fd9f8
Eric Nyandwi <ericnyandwi_803@hotmail.com>
2017-03-30 17:13:47 +0200

    Mon troisieme commit

---
+++ src/entities/MaClassSimple.java
@@ -0,0 +1,10 @@
+package entities;
+
+public class MaClassSimple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+               System.out.println("Salut  toi ");
+	}
+
+}
